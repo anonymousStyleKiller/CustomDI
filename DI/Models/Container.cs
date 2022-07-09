538d68ce2d6a0dcb6e1b76d3285bfe6c357108ae
Anton <gumb1t97@gmail.com>
2022-07-09 20:49:20 +0300

    Validation of DI scoped

--- DI/Models/Container.cs
+++ DI/Models/Container.cs
@@ -1,14 +1,21 @@
-﻿using DI.Interfaces;
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Reflection;
+using DI.Descriptors;
+using DI.Interfaces;
 
 namespace DI.Models;
 
 public class Container : IContainer
 {
-    private Dictionary<Type, ServiceDescriptor> Descriptors { get; }
+    private ImmutableDictionary<Type, ServiceDescriptor> Descriptors { get; }
+    private readonly ConcurrentDictionary<Type, Func<IScope, object>> _buildActivator = new();
+    public readonly Scope _rootScope;
 
     public Container(IEnumerable<ServiceDescriptor> descriptors)
     {
-        Descriptors = descriptors.ToDictionary(x => x.ServiceType);
+        Descriptors = descriptors.ToImmutableDictionary(x => x.ServiceType);
+        _rootScope = new Scope(this);
     }
 
     public IScope CreateScope()
@@ -16,19 +23,38 @@ public class Container : IContainer
         return new Scope(this);
     }
 
-    public object CreateInstance(Type service)
+    private Func<IScope, object> BuildActivation(Type service, IScope scope)
     {
-        if (!Descriptors.TryGetValue(service, out _))
+        if (!Descriptors.TryGetValue(service, out var descriptor))
             throw new InvalidOperationException($"Service {service} is not registered");
 
-        var constructorInfo = service.GetConstructors().Single();
-        var parameters = constructorInfo.GetParameters();
-        var parametersForConstructor = new object[parameters.Length];
-        for (var i = 0; i < parameters.Length; i++)
-        {
-            parametersForConstructor[i] = CreateInstance(parameters[i].ParameterType);
-        }
+        if (descriptor is InstanceBasedServiceDescriptor ib) return _ => ib.Instance;
+        if (descriptor is FactoryBasedServiceDescriptor fb) return _ => fb.Factory(scope);
+        var tb = (TypeBasedServiceDescriptor)descriptor;
 
-        return constructorInfo.Invoke(parametersForConstructor);
+        var constructorInfo =
+            tb.ImplementationType.GetConstructors(BindingFlags.Public | BindingFlags.Instance).Single();
+        var parameters = constructorInfo.GetParameters();
+        return s =>
+        {
+            var parametersForConstructor = new object[parameters.Length];
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                parametersForConstructor[i] = CreateInstance(parameters[i].ParameterType, scope);
+            }
+
+            return constructorInfo.Invoke(parametersForConstructor);
+        };
+    }
+
+    public object CreateInstance(Type service, IScope scope)
+    {
+        return _buildActivator.GetOrAdd(service, type => BuildActivation(type, scope));
+    }
+
+    public ServiceDescriptor FindDescriptor(Type service)
+    {
+        Descriptors.TryGetValue(service, out var result);
+        return result;
     }
 }
--- DI/Models/Scope.cs
+++ DI/Models/Scope.cs
@@ -1,15 +1,28 @@
-﻿using DI.Interfaces;
+﻿using System.Collections.Concurrent;
+using DI.Enums;
+using DI.Interfaces;
 
 namespace DI.Models;
 
 public class Scope : IScope
 {
     private readonly Container _container;
+    private readonly ConcurrentDictionary<Type, object> _scopedInstances = new();
 
     public Scope(Container container)
     {
         _container = container;
     }
 
-    public object Resolve(Type service) => _container.CreateInstance(service);
+    public object Resolve(Type service)
+    {
+        var descriptor = _container.FindDescriptor(service);
+        if (descriptor.LifeTime == LifeTime.Transient)
+            return _container.CreateInstance(service, this);
+        if (descriptor.LifeTime is LifeTime.Scoped || _container._rootScope == this)
+            return _scopedInstances.GetOrAdd(service, s => _container.CreateInstance(s, this));
+        if (descriptor.LifeTime == LifeTime.Singleton)
+            return _container._rootScope.Resolve(service);
+        return _container.CreateInstance(service, this);
+    }
 }

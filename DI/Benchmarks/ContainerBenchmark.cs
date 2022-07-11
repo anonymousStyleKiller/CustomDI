c675e491020fcff4f3e56e55e51c672f83671e9c
Anton <gumb1t97@gmail.com>
2022-07-11 23:19:08 +0300

    Added MSDI and Autofac

--- DI/Benchmarks/ContainerBenchmark.cs
+++ DI/Benchmarks/ContainerBenchmark.cs
@@ -1,17 +1,22 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using Autofac;
+using BenchmarkDotNet.Attributes;
 using DI.Controllers;
 using DI.Extensions;
 using DI.Interfaces;
 using DI.Interfaces.Services;
-using DI.Models;
 using DI.Models.ActivationBuilder;
 using DI.Services;
+using Microsoft.Extensions.DependencyInjection;
+using ContainerBuilder = DI.Models.ContainerBuilder;
 
 namespace DI.Benchmarks;
 
+[MemoryDiagnoser]
 public class ContainerBenchmark
 {
     private readonly IScope _reflectionBased, _lambdaBased;
+    private readonly ILifetimeScope _scope;
+    private readonly IServiceScope _provider;
     public ContainerBenchmark()
     {
       var lambdaBuilder =  new ContainerBuilder(new LambdaActivationBuild());
@@ -20,13 +25,10 @@ public class ContainerBenchmark
       InitController(reflectionBuilder);
       _lambdaBased = lambdaBuilder.Build().CreateScope();
       _reflectionBased = reflectionBuilder.Build().CreateScope();
+      _scope = InitAutofac();
+      _provider = InitMSDI();
     }
-
-    private void InitController(ContainerBuilder builder)
-    {
-        builder.RegisterTransient<IService, Service>()
-            .RegisterTransient<Controller, Controller>();
-    }
+    
     
     [Benchmark(Baseline = true)]
     public Controller Create() => new Controller(new Service());
@@ -36,5 +38,35 @@ public class ContainerBenchmark
     
     [Benchmark(Description = "Lambda")]
     public Controller Lambda() => (Controller)_lambdaBased.Resolve(typeof(Controller));
+
+
+    [Benchmark(Description = "Autofac")]
+    public Controller Autofac() => _scope.Resolve<Controller>();
     
+    [Benchmark(Description = "MSDI")]
+    public Controller MSDI() => _provider.ServiceProvider.GetRequiredService<Controller>();
+
+
+    private ILifetimeScope InitAutofac()
+    {
+       var containerBuilder =  new Autofac.ContainerBuilder();
+       containerBuilder.RegisterType<Service>().As<IService>();
+       containerBuilder.RegisterType<Controller>().AsSelf();
+       return containerBuilder.Build().BeginLifetimeScope();
+    }
+
+    private IServiceScope InitMSDI()
+    {
+        var collection = new ServiceCollection();
+        collection.AddTransient<IService, Service>();
+        collection.AddTransient<Controller, Controller>();
+        return collection.BuildServiceProvider().CreateScope();
+    }
+    
+    
+    private void InitController(ContainerBuilder builder)
+    {
+        builder.RegisterTransient<IService, Service>()
+            .RegisterTransient<Controller, Controller>();
+    }
 }
